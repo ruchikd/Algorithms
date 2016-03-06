93ffad784161204c8e9d87d38255d74df443039c
Ruchik Dave <ruchikd@gmail.com>
2016-03-06 14:45:22 -0800

    Find if string is palindrome
    Check if the string is palindrome

---
+++ Python/FindIfPalindrome/ifIsPalindrome.py
@@ -0,0 +1,13 @@
+
+#This is a string which has some value
+str = 'abbaccaccabba'
+
+"""
+Below is the way how I find out if the string is a plaindrom
+Palindrom is a word which is same when read either way
+"""
+
+if str == str[::-1]:
+	print str + " is a palindrom"
+else:
+	print str + " is not a palindrome"
