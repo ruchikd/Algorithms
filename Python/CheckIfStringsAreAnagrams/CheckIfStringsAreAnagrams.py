dc0dd75c651eaf1cb36149f38860e2d5dbdd7edc
Ruchik Dave <ruchikd@gmail.com>
2016-04-05 23:13:52 -0700

    check if strings are anagrams
    check if strings are anagrams

--- Python/CheckIfStringsAreAnagrams/CheckIfStringsAreAnagrams.py
+++ Python/CheckIfStringsAreAnagrams/CheckIfStringsAreAnagrams.py
@@ -7,18 +7,41 @@ def checkIfAnagrams(str1, str2):
 		print str1, " and ", str2, " are not Anagrams"
 		return
 
-	d = dict()
-	for x in range(len(str1)):
-		d[str1[x]] = True
+	anagramList = [0]*256
+	for x in range(256):
+		anagramList[x] = 0
 
-	for x in range(len(str2)):
-		if str2[x] not in d.keys():
-			print str1, " and ", str2, " are not Anagrams"
+	for w in str1:
+		if (anagramList[ord(w)]):
+			val = anagramList[ord(w)]
+		else:
+			val = 0
+		anagramList[ord(w)] = val + 1
+
+	for w in str2:
+		if (anagramList[ord(w)]):
+			val = anagramList[ord(w)]
+			val = val - 1
+			anagramList[ord(w)] = val
+		else:
+			print str1, "and", str2, "are not Anagrams"
+			return
+
+	for x in anagramList:
+		if x > 0:
+			print str1, "and", str2, "are not Anagrams"
+			return
 
 	print str1, " and ", str2, " are Anagrams"
 
 def main():
 	checkIfAnagrams("Ruchik", "hRicku")
+	checkIfAnagrams("rrr", None)
+	checkIfAnagrams(None, None)
+	checkIfAnagrams("r", "R")
+	checkIfAnagrams("r", "r")
+	checkIfAnagrams('1', '1')
+	checkIfAnagrams('ruu', 'rrr')
 
 if __name__ == '__main__':
 	main()
