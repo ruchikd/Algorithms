af852c739d25a7a63bf1cafc89354eee4f06f5ec
Ruchik Dave <ruchikd@gmail.com>
2016-04-22 22:40:42 -0700

    Longest common substring
    Find Longest common substring from a given string

--- Python/LongestCommonSubstring/LongestCommonSubstring.py
+++ Python/LongestCommonSubstring/LongestCommonSubstring.py
@@ -1,7 +1,7 @@
 def longest_common_substring(s1, s2):
     if s1 is None or s2 is None:
         return
-        
+
     # create a 2-dimensional array of size [len(s2)][len(s1)]
     m = [[0] * (1 + len(s2)) for i in xrange(1 + len(s1))]
     longest, x_longest = 0, 0
@@ -15,10 +15,21 @@ def longest_common_substring(s1, s2):
             else:
                 m[x][y] = 0
 
+    print m
+    print x_longest
+    print longest
+
     return s1[x_longest - longest: x_longest]
 
+    '''
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+    [0, 0, 2, 0, 2, 0, 2, 0, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 3]]
+    '''
+
 def main():
-    print longest_common_substring("abcd", "ebc")
+    print longest_common_substring("bcb", "bcbcbcbca")
 
 if __name__ == '__main__':
     main()
