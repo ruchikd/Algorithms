2c1b35908370baa3915c419c52e49a1b4ac58c12
Ruchik Dave <ruchikd@gmail.com>
2016-04-22 22:39:36 -0700

    2 Sum Problem
    2 Sum Problem

---
+++ Python/2Sum/2Sum.py
@@ -0,0 +1,29 @@
+def find2Sum(arr, target):
+	if arr is None:
+		return None
+
+	sumList = []
+	d = dict()
+
+	for x in range(len(arr)):
+		if arr[x] in d:
+			index = d[arr[x]]
+			smallList = []
+			smallList.append(index)
+			smallList.append(x)
+			sumList.append(smallList)
+		else:
+			print "target =", target, "arr[",x,"] =", arr[x]
+			print "adding target - arr[x] =", target - arr[x], "in dict"
+			d[target - arr[x]] = x
+
+	print sumList
+
+def main():
+	arr = [2, 7, 11, 15]
+	target = 9
+
+	find2Sum(arr, target)
+
+if __name__ == '__main__':
+	main()
