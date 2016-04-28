689dc4a5c253c0971d0d59112b8dbdb0475dd18b
Ruchik Dave <ruchikd@gmail.com>
2016-04-27 22:19:10 -0700

    Ranking system example2
    Ranking system example2

--- Python/RankingSystem/rankingSystem.py
+++ Python/RankingSystem/rankingSystem.py
@@ -3,6 +3,21 @@ class Students:
 		self.name = name
 		self.percentage = percentage
 
+def getPercentile(percentageList):
+	percentageList = sorted(percentageList, reverse=True)
+	print percentageList
+
+	rank = []
+
+	for x in range(len(percentageList)):
+		if percentageList[x][0] != percentageList[x-1][0]:
+			rank.append(x+1)
+		else:
+			rank.append(rank[x-1])
+
+	for x in range(len(percentageList)):
+		print percentageList[x][1], " is having", percentageList[x][0], "percentage with rank as", rank[x]
+
 def getRanking(percentageList):
 	percentageList = sorted(percentageList, key=lambda t: t[1], reverse=True)
 	rank = []
@@ -18,8 +33,10 @@ def getRanking(percentageList):
 
 
 def main():
-	percentageList = [('a', 99), ('b', 99.5), ('c', 96), ('d', 99), ('d', 96), ('e', 96), ('f', 94)]
-	getRanking(percentageList)
+	percentageList = [('a', 99), ('b', 99.5), ('c', 96), ('d', 99), ('e', 96), ('f', 96), ('g', 94)]
+	#getRanking(percentageList)
+	percentageList1 = [(99, 'a'), (99.5, 'b'), (96, 'c'), (99, 'd'), (96, 'e'), (96, 'f'), (94, 'g')]
+	getPercentile(percentageList1)
 
 if __name__ == '__main__':
 	main()
